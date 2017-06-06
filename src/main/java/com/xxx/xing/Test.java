df96d8f829aab413bc79f52727c67a0a830b31ea
xiexingxing <906877230@qq.com>
2017-06-06 10:19:17 +0800

    kt 删除

--- src/main/java/com/xxx/xing/Test.java
+++ src/main/java/com/xxx/xing/Test.java
@@ -1,9 +1,17 @@
 package com.xxx.xing;
 
+import cn.edu.hfut.dmic.contentextractor.ContentExtractor;
+import com.xxx.xing.entity.Bookmark;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 /**
  * @author xing
@@ -13,24 +21,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/test")
 public class Test {
 
-//    @RequestMapping("/jsoup")
-//    @ResponseBody
-//    public Bookmark test(@RequestParam(name = "url", required = false) String url) throws IOException {
-//        Connection connection = Jsoup.connect(url);
-//        connection.timeout(5000).userAgent("Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
-//        Document doc = connection.get();
-//        HttpRequest request = new HttpRequest(url);
-//        String html = request.response().decode();
-//        Document doc = Jsoup.parse(html, url);
-//        String title=doc.title();
-//        String content= ContentExtractor.getContentByUrl(url);
-//
-//
+    @RequestMapping("/jsoup")
+    @ResponseBody
+    public Bookmark test(@RequestParam(name = "url", required = false) String url) throws IOException {
+        Connection connection = Jsoup.connect(url);
+        connection.timeout(5000).userAgent("Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
+        Document doc = connection.get();
+
 //        DomPage domPage = new DomPage(doc);
 //        ContentExtractor contentExtractor = new ContentExtractor(domPage);
 //        String content = Jsoup.parse(contentExtractor.getContent()).text();
 //        return new Bookmark("12", domPage.getDoc().title(), content, url);
-//    }
+        try {
+            String content= ContentExtractor.getContentByUrl(url);
+        return new Bookmark("12", "21", content, url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
     @Value("${solr.host}")
     String solr;
 
